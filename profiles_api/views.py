6f5a025eac9a62e3c2be6292f794adbecd020283
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2024-07-09 12:21:25 +0530

    API views init

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        an_apiview = [
+            'Use http methods as functions',
+            'it is similar to traditional views. only used for building apis'
+        ]
+
+        return Response({
+            'message': 'Hello',
+            'api_view': an_apiview
+        })
+
+
+
